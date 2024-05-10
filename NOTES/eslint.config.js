2e70f179138b18d25c52727e03b5aff810d407d7
Franklin B. <43481761+Franklin-Barbosa@users.noreply.github.com>
2024-05-10 12:26:25 +0000

    Adicionando comentários no eslint.config

--- NOTES/eslint.config.js
+++ NOTES/eslint.config.js
@@ -1,4 +1,5 @@
-/*import globals from "globals";
+/*
+import globals from "globals";
 import pluginJs from "@eslint/js";
 
 
@@ -9,7 +10,9 @@ export default [
 */
 
 module.exports = {
+  // configuração se aplica a todo o projeto.
   root: true,
+  // ambiente (navegador e ES2020).
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
@@ -17,15 +20,18 @@ module.exports = {
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
   ],
+  // exclui diretórios ou arquivos da verificação de lint.
   ignorePatterns: ["dist", ".eslintrc.cjs"],
+  // versão ECMAScript e o tipo de origem.
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  // configurações adicionais relacionadas ao React.
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh", "security"],
   rules: {
     "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-    "react/prop-types": "off",
-    "no-warning-comments": "off",
-    "security/detect-non-literal-regexp": "warn",
-    "security/detect-buffer-noassert": "warn",
+    "react/prop-types": "off", // desativa a necessidade de declarar os tipos de prop em componentes React.
+    "no-warning-comments": "off", // desativa avisos para comentários contendo palavras-chave específicas.
+    "security/detect-non-literal-regexp": "warn", //avisa sobre expressões regulares não literais.
+    "security/detect-buffer-noassert": "warn", // avisa sobre o uso de Buffer sem verificações.
   },
 };
