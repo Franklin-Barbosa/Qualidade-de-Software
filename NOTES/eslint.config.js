7714f391cc7305b06a2eead5c5fee37c33c9bfb9
Franklin B. <43481761+Franklin-Barbosa@users.noreply.github.com>
2024-05-10 12:20:01 +0000

    eslint.config.js update...

--- NOTES/eslint.config.js
+++ NOTES/eslint.config.js
@@ -1,4 +1,4 @@
-import globals from "globals";
+/*import globals from "globals";
 import pluginJs from "@eslint/js";
 
 
@@ -6,4 +6,26 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
 ];
+*/
 
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh", "security"],
+  rules: {
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "react/prop-types": "off",
+    "no-warning-comments": "off",
+    "security/detect-non-literal-regexp": "warn",
+    "security/detect-buffer-noassert": "warn",
+  },
+};
